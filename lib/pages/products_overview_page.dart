b5e06e832faef999be48eeb9e7623faa8143782d
Sant'Clear <santclear@gmail.com>
2023-03-22 23:18:20 -0300

    03 - Created ProductsOverviewPage widget and set it as the home page.

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('My Store'),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          itemCount: loadedProducts.length,
+        ),
+      ),
+    );
+  }
+}
