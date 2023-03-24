ebeb7609592ea1040399ed09a44d04f868b3be47
Sant'Clear <santclear@gmail.com>
2023-03-23 22:38:45 -0300

    09 - Fixed provider dependency and extracted product grid to a new widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,37 +1,20 @@
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Center(
           child: Text('My Store'),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-          itemCount: loadedProducts.length,
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(10),
+        child: ProductGrid(),
       ),
     );
   }
