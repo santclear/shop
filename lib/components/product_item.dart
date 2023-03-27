5107ab2ff1f3f64a20fc52bd839b9a89e6525de4
Sant'Clear <santclear@gmail.com>
2023-03-27 00:14:52 -0300

    14 - Created CartItem widget.

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -26,7 +26,7 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           title: Text(
-            product.title,
+            product.name,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
