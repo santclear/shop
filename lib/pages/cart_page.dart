0325e10c73ad72f6174ce446ac4877c4bb5cb052
Sant'Clear <santclear@gmail.com>
2023-03-28 10:47:24 -0300

    19 - Displaying total value of products in CartPage widget and styled the page.

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    label: Text(
+                      'R\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6
+                              ?.color),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      textStyle:
+                          TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                    child: const Text('Buy'),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
