1dad3d834aa82ce28d6693fe069cdd888ecc6c41
Sant'Clear <santclear@gmail.com>
2023-03-28 09:39:44 -0300

    18 - Created CartPage widget and added navigation to it.

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/components/badge.dart';
 import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/utils/app_routes.dart';
 
 enum FilterOptions {
   Favorite,
@@ -51,7 +52,9 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
           ),
           Consumer<Cart>(
             child: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.CART);
+              },
               icon: const Icon(Icons.shopping_cart),
             ),
             builder: (ctx, cart, child) =>
