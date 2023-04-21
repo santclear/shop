70def36bd00c1f0c93b89452fc57c75d691bbcbb
santclear <santclear@gmail.com>
2023-04-21 18:17:02 -0300

    47 - Allows to remove product in memory through the remove button.

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -34,7 +36,12 @@ class ProductItem extends StatelessWidget {
               icon: const Icon(Icons.edit),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<ProductList>(
+                  context,
+                  listen: false,
+                ).removeProduct(product);
+              },
               color: Theme.of(context).colorScheme.error,
               icon: const Icon(Icons.delete),
             ),
