f554ad1cf32c12cdeb4bd53826b2525bf05b1956
santclear <santclear@gmail.com>
2023-04-21 18:35:56 -0300

    48 - Displays confirmation message when attempting to delete a product.

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -37,10 +37,30 @@ class ProductItem extends StatelessWidget {
             ),
             IconButton(
               onPressed: () {
-                Provider.of<ProductList>(
-                  context,
-                  listen: false,
-                ).removeProduct(product);
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Delete ${product.name}'),
+                    content: Text('Would you like to confirm deletion ${product.name}?'),
+                    actions: [
+                      TextButton(
+                        child: const Text('No'),
+                        onPressed: () => Navigator.of(ctx).pop(false),
+                      ),
+                      TextButton(
+                        child: const Text('Yes'),
+                        onPressed: () => Navigator.of(ctx).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).removeProduct(product);
+                  }
+                });
               },
               color: Theme.of(context).colorScheme.error,
               icon: const Icon(Icons.delete),
