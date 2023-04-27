84e1d8d22de58d1e5539151dfbecc4d7ab02232a
santclear <santclear@gmail.com>
2023-04-26 22:33:35 -0300

    54 - Products are loaded from the database and listed.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -3,12 +3,11 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _baseUrl = 'https://shop-595d5-default-rtdb.firebaseio.com/';
-  final List<Product> _items = dummyProducts;
+  final _url = 'https://shop-595d5-default-rtdb.firebaseio.com/products.json';
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
 
@@ -17,6 +16,24 @@ class ProductList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadProducts() async {
+    final response = await http.get(Uri.parse(_url));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    if(response.body == 'null') return;
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
   }
 
   Future<void> saveProductFromData(Map<String, Object> data) {
@@ -38,7 +55,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/products.json'),
+      Uri.parse(_url),
       body: jsonEncode(
         {
           'name': product.name,
