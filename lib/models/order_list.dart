8b44f223ba86efb4aaac2cea4802a6326e71b4ee
santclear <santclear@gmail.com>
2023-04-27 23:12:57 -0300

    61 - Loads orders and their respective details from the database.

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -16,6 +17,37 @@ class OrderList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+    );
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    if (response.body == 'null') return;
+
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+          date: DateTime.parse(orderData['date']),
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> addOrder(Cart cart) async {
