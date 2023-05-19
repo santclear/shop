a46f0048520928643bc62ade6f409b3f4bda3544
santclear <santclear@gmail.com>
2023-05-18 23:03:20 -0300

    77 - Automatic login and local persistence.

--- lib/pages/auth_or_home_page.dart
+++ lib/pages/auth_or_home_page.dart
@@ -10,6 +10,22 @@ class AuthOrHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.error != null) {
+          return const Center(
+            child: Text('An error has occurred!'),
+          );
+        } else {
+          return auth.isAuth ? const ProductsOverviewPage() : const AuthPage();
+        }
+      },
+    );
   }
 }
